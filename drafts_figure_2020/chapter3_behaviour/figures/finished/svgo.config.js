d19ede9505b5e260eef9a66e519bb08b829da848
RobWHickman <robwhickman@gmail.com>
2026-08-30 16:07:16 +0100

    roll drafts_figure_2020 into repo, drop nested git

---
+++ drafts_figure_2020/chapter3_behaviour/figures/finished/svgo.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    {
+      name: "preset-default",
+      params: {
+        overrides: {
+          collapseGroups: false,
+          mergePaths: false,
+          convertShapeToPath: false
+        },
+      },
+    },
+  ],
+};
